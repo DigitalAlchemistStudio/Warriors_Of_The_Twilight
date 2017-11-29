e3410b1ec654762aa82edd2d4230aa930e6fdb15
Cory Shumate <cory.shumate@digitalalchemists.co>
2017-11-28 23:05:39 -0700

    Add V1 Project

---
+++ Warriors of the Twilight/V1/Assets/Scripts/Map.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+
+
+	public GameObject Square;
+
+	// Size of the map in terms of number of tiles
+	// This is NOT representative of the amount of
+	// world space that we're going to take up
+	// (i.e. our tiles might be more or less than 1 Unity World Unit)
+
+	int width = 20;
+	int height = 20;
+
+
+	// Use this for initialization
+	void Start () 
+    {
+
+        for (int x = 0; x < width; x++)
+            for (int y = 0; y <height; y++)
+            {
+
+            }
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Warriors of the Twilight/V1/Assets/Scripts/Map.js
@@ -0,0 +1,29 @@
+﻿public var tilePrefab: GameObject;
+public var numberOfTiles: int = 10;
+public var distanceBetweenTiles: float = 1.0;
+public var tilesPerRow: int = 4;
+
+function Start()
+{
+    CreateTiles();
+
+}
+ 
+function CreateTiles()
+{
+  var xOffset: float = 0.0;
+    var zOffset: float = 0.0;
+ 
+    for(var tilesCreated: int = 0; tilesCreated < numberOfTiles; tilesCreated += 1)
+    {
+        xOffset += distanceBetweenTiles;
+         
+        if(tilesCreated % tilesPerRow == 0)
+        {
+            zOffset += distanceBetweenTiles;
+            xOffset = 0;
+        }
+     
+        Instantiate(tilePrefab, Vector3(transform.position.x + xOffset, transform.position.y, transform.position.z + zOffset), transform.rotation);
+    }
+}
