9ac4f153b791a3a56895f4e07789d32d3a760bf2
Cory Shumate <cory.shumate@digitalalchemists.co>
2017-11-29 18:39:32 -0700

    V1

---
+++ Warriors of the Twilight/V1/Assets/Scripts/Camera and Cursor Movement.cs
@@ -0,0 +1,26 @@
+﻿/*
+* Copyright (c) Digital Alchemists
+*/
+
+using UnityEngine;
+
+public class CameraandCursorMovement : MonoBehaviour {
+	
+#region Variables
+#endregion
+
+#region Unity Methods
+#endregion Unity
+	
+    // Use this for initialization
+
+	void Start () 
+{
+		
+	}
+
+	void Update () 
+{
+		
+	}
+}
